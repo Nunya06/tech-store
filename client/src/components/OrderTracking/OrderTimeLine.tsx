beb60655efc937ac3002ee61cdb33c43a90f4d30
Xorse Wisdom <wisdomxorse928@gmail.com>
2026-08-15 22:55:09 +0000

    Style update

--- client/src/components/OrderTracking/OrderTimeLine.tsx
+++ client/src/components/OrderTracking/OrderTimeLine.tsx
@@ -15,7 +15,7 @@ export default function OrderTimeLine({ order }: { order: any }) {
 
     return (
         <div className="bg-white rounded-2xl p-3">
-            <h2 className="text-lg font-semibold text-app-orange-dark mb-6">Delivery Progress</h2>
+            <h2 className="text-lg font-semibold text-app-black mb-6">Delivery Progress</h2>
             <div className="space-y-0">
                 {allStatuses.map((status, i) => {
                     const Icon = statusIcons[status] || PackageIcon;
