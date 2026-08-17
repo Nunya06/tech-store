98850f1027aea571a9118e13a7443e30b8137050
Xorse Wisdom <wisdomxorse928@gmail.com>
2026-08-17 14:02:38 +0000

    li added

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -54,9 +54,9 @@ const Footer = () => {
                             {footerData.contact.map((item, i) => {
                                 const Icon = item.icon;
                                 return (
-                                    <a key={i} className="flex gap-3 text-sm text-app-black/70">
+                                    <li key={i} className="flex gap-3 text-sm text-app-black/70">
                                         <Icon className="size-4 text-app-black" /> {item.text}
-                                    </a>
+                                    </li>
                                 );
                             })}
                         </ul>
